97212584210f9c4362944ae5d691be00f46d7771
Daniel James Smith <djsmith@web.de>
2022-03-14 12:05:56 +0100

    Comments and console-logs

--- src/background/runtime.background.ts
+++ src/background/runtime.background.ts
@@ -118,6 +118,8 @@ export default class RuntimeBackground {
         await this.main.reseedStorage();
         break;
       case "collectPageDetailsResponse":
+        console.log("processMessage msg", msg);
+        console.log("processMessage sender", sender);
         switch (msg.sender) {
           case "autofiller":
           case "autofill_cmd": {
